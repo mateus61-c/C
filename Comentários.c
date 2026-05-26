604d394d7b94f216b58983e5d70dd5ff4ffd5652
mateus61-c <mateusviana4361@gmail.com>
2026-05-26 17:09:15 +0000

    Update Comentários.c

--- Comentários.c
+++ Comentários.c
@@ -1,25 +1,12 @@
 #include <stdio.h>
 
-/*
-   Programa: Exemplo de Comentários
-   Autor: Desenvolvedor C
-   Descrição: Demonstra o uso de comentários de 
-   múltiplas linhas e de linha única.
-*/
-
 int main() {
-    // Declarando variáveis para o cálculo
-    int numero1 = 10;
-    int numero2 = 20;
-    int soma;
+    
+    // Isso e um comentario de uma linha
 
-    // Realizando a soma dos dois números
-    soma = numero1 + numero2;
-
-    /* A função printf vai exibir o resultado 
-       formatado na tela do usuário.
+    /* Isso e um comentario de 
+       multiplas linhas 
     */
-    printf("A soma de %d e %d e igual a: %d\n", numero1, numero2, soma);
 
-    return 0; // Retorna 0 para indicar que o programa terminou com sucesso
+    return 0;
 }
