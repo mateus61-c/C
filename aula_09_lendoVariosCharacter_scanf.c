40371ab0678effc94f0affd737e84ad0a088ccae
mateus61-c <mateusviana4361@gmail.com>
2026-05-27 13:35:49 +0000

    Create aula_09_lendoVariosCharacter_scanf.c

---
+++ aula_09_lendoVariosCharacter_scanf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int num1, num2, num3;
+
+  printf("digite 3 valores: ");
+  scanf("%d%d%d", &num1, &num2, &num3);
+  printf("valores lidos do teclado %d %d% d\n", num1, num2, num3);
+  return 0;
+}
