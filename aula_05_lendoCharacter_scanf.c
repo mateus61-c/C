abc818fa11ea914f89dc4f4a525c3040084b41f9
mateus61-c <mateusviana4361@gmail.com>
2026-05-27 01:20:02 -0300

    Create aula_05_lendoCharacter_scanf.c

---
+++ aula_05_lendoCharacter_scanf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // variavel do tipo char 
+    char sexo;
+    
+    printf("digite seu sexo (F, f, M, m) ");
+    
+    // lendo variavel do tipo char
+    scanf("%c", &sexo);
+    
+    // imprimir variavel do tipo char
+    printf("%c", sexo);
+    
+    return 0;
+}
