e43470714d2b28085634ff08db294cb6fc307240
mateus61-c <mateusviana4361@gmail.com>
2026-05-27 04:01:27 +0000

    Update aula_04_leituradeNumerosReais_scanf.c

--- aula_04_leituradeNumerosReais_scanf.c
+++ aula_04_leituradeNumerosReais_scanf.c
@@ -2,20 +2,20 @@
 
 int main(void) {
   // reverva espaco para 2 variaveis inteiras
-  int num1, num2;
+  float num1, num2;
 
   // atribuir valor a variavel
-  num1 = 10;
-  num2 = 20;
+  num1 = 2,15;
+  num2 = 7,54;
 
   // imprimir o valor real das variaveis
-  printf("%d\n%d\n", num1, num2);
+  printf("%f\n%f\n", num1, num2);
 
   printf("digite o valor de num1 e num2 ");
 
   // sobrescreve o valor das variaveis
-  scanf("%d%d", &num1, &num2);
+  scanf("%f%f", &num1, &num2);
   // imprimi o valor das variaveis ja sobrescritas
-  printf("%d\n%d\n", num1, num2);
+  printf("%f\n%f\n", num1, num2);
   return 0;
 }
